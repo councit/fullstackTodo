d5399c8b381ccb2022d5c7af05b52920ebce0f9a
councit <councit@gmail.com>
2020-12-04 01:27:00 -0800

    first push

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//Set up default mongoose connection
+var mongoDB = "mongodb://127.0.0.1/todo";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api", routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
